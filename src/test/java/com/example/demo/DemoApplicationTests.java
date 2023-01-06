c4dbe7be57020a9762d6baea400cd6d569868bed
ch <mkmsacc@outlook.com>
2023-01-06 14:49:26 +0700

    Add test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,41 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.server.WebHandler;
 
-@SpringBootTest
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ExtendWith(RestDocumentationExtension.class)
 class DemoApplicationTests {
+    @Autowired
+    private WebTestClient webTestClient;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation) {
+        this.webTestClient = WebTestClient.bindToController(DemoController.class)
+                .configureClient()
+                .filter(documentationConfiguration(restDocumentation))
+                .build();
+    }
 
+    @Test
+    public void sample() {
+        this.webTestClient.get()
+                .uri("/")
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBody(String.class)
+                .consumeWith(document("demo"));
+    }
 }
